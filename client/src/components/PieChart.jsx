7d38532136dcb5c3ede1baeaf1e3679440129895
Mark Du <dulongjie@gmail.com>
2022-06-09 14:22:34 -0400

    sample pie chart

--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -10,5 +10,42 @@ const coins = [
 ];
 
 export default function PieChart() {
-  return <div>PieChart</div>;
+  const [active, setActive] = useState(null);
+  const width = 400;
+  const half = width / 2;
+
+  return (
+    <div>
+      <svg width={width} height={width}>
+        <Group top={half} left={half}>
+          <Pie
+            data={coins}
+            pieValue={(data) => data.amount * data.inUSD}
+            outerRadius={half}
+            innerRadius={({ data }) => {
+              const size = active && active.sym === data.sym ? 20 : 16;
+              return half - size;
+            }}
+            padAngle={0.01}
+          >
+            {(pie) =>
+              pie.arcs.map((arc) => (
+                <g
+                  key={arc.data.sym}
+                  onMouseEnter={() => setActive(arc.data)}
+                  onMouseLeave={() => setActive(null)}
+                >
+                  <path d={pie.path(arc)} fill={arc.data.color}></path>
+                </g>
+              ))
+            }
+          </Pie>
+          <Text textAnchor="middle" dy={-30}>
+            Hello
+          </Text>
+          <Text textAnchor="middle">KKK</Text>
+        </Group>
+      </svg>
+    </div>
+  );
 }
